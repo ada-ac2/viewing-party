30c7f3cc850e99659e88d002d7f1c6816d92eaca
kelly <kelly.tate.dev@gmail.com>
2022-10-27 08:13:35 -0400

    Passes Wave 3 Test 4

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -43,7 +43,7 @@ def test_friends_unique_movies():
     assert FANTASY_4 in friends_unique_movies
     assert amandas_data == clean_wave_3_data()
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_friends_unique_movies_not_duplicated():
     # Arrange
     amandas_data = clean_wave_3_data()
@@ -55,11 +55,12 @@ def test_friends_unique_movies_not_duplicated():
     # Assert
     assert len(friends_unique_movies) == 3
 
-    raise Exception("Test needs to be completed.")
+    # raise Exception("Test needs to be completed.")
     # *************************************************************************************************
     # ****** Add assertions here to test that the correct movies are in friends_unique_movies **********
-    # **************************************************************************************************
-
+    # ************************************************
+    
+    assert len(set(movie["title"] for movie in friends_unique_movies)) == len(friends_unique_movies)
 @pytest.mark.skip()
 def test_friends_not_unique_movies():
     # Arrange
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -80,7 +80,7 @@ def get_friends_unique_watched(user_data):
     friends_unique_movies = []
     for friend in user_data["friends"]:
         for movie_entry in friend["watched"]:
-            if movie_entry not in user_data["watched"]:
+            if movie_entry not in user_data["watched"] and movie_entry not in friends_unique_movies:
                 friends_unique_movies.append(movie_entry)
 
     return friends_unique_movies
