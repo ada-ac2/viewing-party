0fa444709d6bbdca0d8222fec0ec3aeeb4ab0798
kelly <kelly.tate.dev@gmail.com>
2022-10-27 05:25:16 -0400

    Wave 1 Tests Passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,12 +1,40 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    
+    if title == None or genre == None or rating == None:
+        return None
+    else:
+    
+        new_movie = {
+            "title": title,
+            "genre": genre,
+            "rating": rating
+        }
 
+        return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie):
+
+    for i in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][i]["title"] == movie:
+            user_data["watched"].append(user_data["watchlist"][i])
+            user_data["watchlist"].remove(user_data["watchlist"][i])
+
+    return user_data
+    
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-
+    
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
