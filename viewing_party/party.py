f31537816c4632de23f079fb23ddcb6cdc4d3bd6
maple-megan333 <maple.megan333@gmail.com>
2022-11-02 13:56:23 -0700

    Completed First wave of testing/ program creation

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,35 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    print("***************************")
+    new_movie = {
+        'title': title,
+        'genre': genre,
+        'rating' : rating
+    }
+    for value in new_movie.values():
+        if value is None:
+            return None
+    return new_movie
 
+def add_to_watchlist(user_data, movie):
+    print("*******************************")
+    if "watchlist" in user_data:
+        user_data["watchlist"].append(movie)
+    return user_data
+
+def add_to_watched(user_data, movie):
+    print("*******************************")
+    if "watched" in user_data:
+        user_data["watched"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie_title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == movie_title:
+            user_data = add_to_watched(user_data, movie)
+            user_data["watchlist"].remove(movie)
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
