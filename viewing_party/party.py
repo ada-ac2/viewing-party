62717b3a55513fa7cc8f7e77096be0e3b7f612e5
maple-megan333 <maple.megan333@gmail.com>
2022-11-02 16:35:33 -0700

    Wave 4 complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -86,6 +86,15 @@ def get_friends_unique_watched(user_data):
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
+    friends_unique_movies = get_friends_unique_watched(user_data)
+    available_reccomended = []
+    for movie in friends_unique_movies:
+        if movie["host"] in user_data["subscriptions"]:
+            available_reccomended.append(movie)
+    return available_reccomended
+
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
