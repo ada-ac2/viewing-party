7752b8f5243f4a05dfc1e405abae78ad3537a8d0
Xuan Hien Pham <phamxuanhien02@gmail.com>
2022-11-03 21:35:50 -0400

    change wave 01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,35 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie = dict()
+    if title == False or genre == False or rating == False:
+        return None
+    movie["title"] = title
+    movie["genre"] = genre
+    movie["rating"] = rating
+    return movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"] = []
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"] = []
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    for movie in user_data["watchlist"]:
+        if title in movie:
+            user_data["watched"] = movie
+            user_data["watchlist"].remove(movie)
+            return user_data
+        
+    return user_data
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
