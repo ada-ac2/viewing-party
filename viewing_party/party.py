d68107c1a85bb23d84113bb85d804afa8efdd137
Xuan Hien Pham <phamxuanhien02@gmail.com>
2022-11-05 18:40:21 -0400

    wave 02 passed test_wave_02

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -31,13 +31,38 @@ def watch_movie(user_data, title):
         
     return user_data
 
-
-
-
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    average_rating = 0.0
+    rating = 0.0
+
+    if len(user_data["watched"]) == 0:
+        return average_rating
+
+    for movie in user_data["watched"]:
+        rating += movie["rating"]
+    average_rating = rating / len(user_data["watched"])
+    return average_rating
+
+def get_most_watched_genre(user_data):
+    if len(user_data["watched"]) == 0:
+        return None
+    genre_frequently = dict()
+    max = 0
+    most_watched_genre = ""
+    for movie in user_data["watched"]:
+        if movie["genre"] in genre_frequently:
+            genre_frequently[movie["genre"]] += 1
+        else:
+            genre_frequently[movie["genre"]] = 1
+        
+        if max < genre_frequently[movie["genre"]]:
+            max = genre_frequently[movie["genre"]]
+            most_watched_genre = movie["genre"]
+    return most_watched_genre        
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
