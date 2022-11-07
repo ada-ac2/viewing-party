94f5c0258f9babd402ca2eb5e8bd51e90008ade4
annadu <duyuan0921@gmail.com>
2022-11-07 11:02:19 -0800

    wave 1 done

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,56 @@
 # ------------- WAVE 1 --------------------
-
+movie = {}
+watched_movie_lst = []
+watch_lst = []
 def create_movie(title, genre, rating):
-    pass
+    if  title and genre and rating :
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+        return movie
+    elif title or genre or rating == False:
+        return None
+
+
+def add_to_watched(user_data, movie):
+    user_data["watched"] = watched_movie_lst
+    watched_movie_lst.append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"] = watch_lst
+    watch_lst.append(movie)
+    return user_data 
+
+def watch_movie(user_data, title):
+    for i in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][i]["title"] == title:
+            movie_change = user_data["watchlist"][i]
+            user_data["watchlist"].pop(i)
+            user_data["watched"].append(movie_change)
+    return user_data  
+           
+           
+
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
