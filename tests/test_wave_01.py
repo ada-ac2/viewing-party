f538c143963bf292f86edeb9b6de7f747ca76417
annadu <duyuan0921@gmail.com>
2022-11-07 11:02:01 -0800

    complete assert

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -1,6 +1,6 @@
 import pytest
 # NOTE: In production code, we developers should change import * to something more specific. Due to some constraints of this project, we will import * in our test files.
-# from viewing_party.main import *
+#from viewing_party.main import *
 from viewing_party.party import *
 from tests.test_constants import *
 
@@ -119,7 +119,12 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
+    #raise Exception("Test needs to be completed.")
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
+
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
@@ -142,8 +147,10 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    
-    raise Exception("Test needs to be completed.")
+    assert movie_to_watch not in updated_data["watchlist"]
+    assert movie_to_watch in updated_data["watched"]
+    #raise Exception("Test needs to be completed.")
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
