7baddec4af685ae4f30202e28a0d388546037c61
Diana Arreola <arreoladiana11@gmail.com>
2022-11-07 12:46:23 -0800

    Add add_to_watchlist func

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -7,10 +7,18 @@ def create_movie(title, genre, rating):
     new_movie = {"title": title, "genre": genre, "rating": rating}
     return new_movie
 
+
 def add_to_watched(user_data, movie):
     if not bool(movie):
         return user_data
     user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    if not bool(movie):
+        return user_data
+    user_data["watchlist"].append(movie)
     return user_data
 
 # -----------------------------------------
