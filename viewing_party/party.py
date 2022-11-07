897a6d410380e06783ee47c75c8418177d908690
Diana Arreola <arreoladiana11@gmail.com>
2022-11-07 12:28:10 -0800

    Add wave1 create_movie and add_to_watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,17 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not bool(title) or not bool(genre) or not bool(rating):
+        return None
+    
+    new_movie = {"title": title, "genre": genre, "rating": rating}
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    if not bool(movie):
+        return user_data
+    user_data["watched"].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
