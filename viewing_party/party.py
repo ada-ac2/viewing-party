7cbb358805ec1a8f575d95b25191476c549e788f
dalia <dnabil89@gmail.com>
2022-11-08 14:45:34 -0800

    wave 1 completed:finished watch_movie function and passing all tests for first wave

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,6 +38,13 @@ def add_to_watchlist(user_data, movie):
     return user_data
 
 
+def watch_movie(user_data, movie_title):
+    for movie in user_data["watchlist"]:
+        if movie_title in movie.values():
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
