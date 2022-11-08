4e177ae5617b8e027410a0ed4201c8e5e03f49db
dalia <dnabil89@gmail.com>
2022-11-08 13:50:43 -0800

    in wave 1: completed functions create_movie and add_to_watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,39 @@
 # ------------- WAVE 1 --------------------
 
-def create_movie(title, genre, rating):
-    pass
+def create_movie(movie_title, genre, rating):
+    movies = {}
+
+    if movie_title == None or genre == None or rating == None :
+        return None
+
+    movies["title"] = movie_title
+    movies["genre"] = genre
+    movies["rating"] = rating
+
+    return movies
+
+movie_title = "MOVIE_TITLE_1"
+genre = "GENRE_1"
+rating = "RATING_1"
+
+# print(create_movie(movie_title, genre, rating))
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+# movie = {
+#     "title": "MOVIE_TITLE_1",
+#     "genre": "GENRE_1",
+#     "rating": "RATING_1"
+# }
+# user_data = {
+#     "watched": []
+# }
+
+# print(add_to_watched(user_data, movie))
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
